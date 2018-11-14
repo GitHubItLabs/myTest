66c1fe204d0f3d277a6782a89bfde1fc03f9642a
Nikola <nikola.kuzmanovski@it-labs.com>
2018-11-14 16:57:36 +0100

    Changed default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { AuthenticateGuard } from './auth/guards/authenticate.guard';
 
 export const PUBLIC_ROUTES: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
 ];
 
